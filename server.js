ba6871240c99506834397f90dac815047b289b69
bpmryan <bpm_ryand2428@gmail.com>
2026-03-26 14:38:10 -0500

    add step 5

--- server.js
+++ server.js
@@ -6,59 +6,61 @@ import { fileURLToPath } from "url";
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  // step 2
-  // Log the request
-  // console.log(`${req.method} ${req.url}`);
-
-  // Send a response
-  // res.writeHead(200, {
-  //     'Content-Type': 'text/html'
-  // });
-  // res.end('<h1>Hello World!</h1>');
-
-  // step 3
+const server = http.createServer(async (req, res) => {
+  // step 3: adding routes
   const url = req.url;
   const method = req.method;
 
   // Route handling
-  if (url === "/" && method === "GET") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>About Page<h1>");
-  } else if (url === "/api/data" && method === "GET") {
+//   if (url === "/" && method === "GET") {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.end("<h1>About Page<h1>");
+//   } else if (url === "/api/data" && method === "GET") {
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(JSON.stringify({ message: "Hello", timestamp: Date.now() }));
+//   } else {
+//     // 404 not found
+//     res.writeHead(404, { "Content-Type": "text/html" });
+//     res.end("<h1>404 - Page Not Found</h1>");
+//   }
+
+  //step 5: route to static files
+  if (url === "/" || url === "index.html") {
+    await serveFile(
+      path.join(dirname, "public", "index.html"),
+      "text/html",
+      res,
+    );
+  } else if (url === "/about") {
+    await serveFile(
+      path.join(dirname, "public", "about.html"),
+      "text/html",
+      res,
+    );
+  } else if (url === "/style.css") {
+    await serveFile(
+      path.join(__dirname, "public", "style.css"),
+      "text/css",
+      res,
+    );
+  } else if (url.startsWith("/images/")) {
+    const ext = path.extname(url);
+    const contentType = ext === ".png" ? "image/png" : "image/jpeg";
+    await serveFile(path.join(__dirname, "public", url), contentType, res);
+  } else if (url === "/api/data") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Hello", timestamp: Date.now() }));
+    res.end(
+      JSON.stringify({ message: "Hello!", time: new Date().toISOString() }),
+    );
   } else {
-    // 404 not found
     res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h1>404 - Page Not Found</h1>");
+    res.end("<h1>404 - Not Found</h1>");
   }
 });
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
-// Step 3: adding routes
-// const server = http.createServer((req, res) => {
-//     const url = req.url;
-//     const method = req.method;
-
-//     // Route handling
-//     if (url === '/' && method === 'GET') {
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         res.end('<h1>About Page<h1>');
-//     }
-//     else if (url === '/api/data' && method === 'GET') {
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({ message: 'Hello', timestamp: Date.now() }) );
-//     }
-//     else {
-//         // 404 not found
-//         res.writeHead(404, { 'Content-Type': 'text/html' });
-//         res.end('<h1>404 - Page Not Found</h1>');
-//     }
-// });
 
 // step 4: serving static files
 const filename = fileURLToPath(import.meta.url);
